e6191172cfe6f47aa9b48ac018f476a71c408b10
j91981274-sudo <j91981274@gmail.com>
2026-05-02 15:13:15 +0530

    ready for deploy

--- app.py
+++ app.py
@@ -120,4 +120,8 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+    socketio.run(
+        app,
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
